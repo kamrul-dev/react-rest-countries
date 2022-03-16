e100ee5b876b3dbcb252f9efaab78a59cffbe55c
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-16 21:41:51 +0600

    load countries data and create a component called LoadCountries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadCountries></LoadCountries>
     </div>
   );
+}
+
+// load countries data and create a component called LoadCountries
+function LoadCountries() {
+  const [countries, setCountries] = useState([]);
+  console.log(countries);
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then(res => res.json())
+      .then(data => setCountries(data));
+  }, [])
+  return (
+    <div>
+      <h1>Visiting Every Country of the World !</h1>
+      <h3>Available Countries: {countries.length}</h3>
+    </div>
+  )
 }
 
 export default App;
