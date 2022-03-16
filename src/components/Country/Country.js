0b80c0a23e149920abdd5298b0fe0cde472f6cb6
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-17 01:50:14 +0600

    bootstrap css added

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -5,7 +5,7 @@ const Country = (props) => {
     // console.log(props.country);
     const {area, region, population, name, flags} = props.country;
     return (
-        <div className='country'>
+        <div className='country bg-warning'>
             <h2>Country Name: {name.common}</h2>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
